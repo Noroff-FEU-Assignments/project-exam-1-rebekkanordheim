cdfe3055c744d3b19115c1e33583dba6e88cd825
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-05-09 11:17:15 +0200

    blog posts

--- js/fetchAll.js
+++ js/fetchAll.js
@@ -1,8 +1,24 @@
-async function getAllPosts() {
+//making a call to fetch the blog posts from the API
+async function getPosts() {
+    const response = await fetch("https://www.rebekkanordheim.no/wp-json/wp/v2/posts?per_page=12");
+    return await response.json();
+  }
+  
+  //console logging the data to check if it calls right
+  async function myPosts() {
+    const posts = await getPosts();
+    console.log(posts);
+  }
+  
+  myPosts();
+  
+// fetching all the available posts
+  async function getAllPosts() {
   const response = await fetch('https://www.rebekkanordheim.no/wp-json/wp/v2/posts');
   return await response.json();
 }
 
+//creating the HTML for each post that gets called from the API
 function createPostsHTML(posts) {
   const container = document.querySelector('.container');
 
@@ -27,5 +43,5 @@ async function allPostsPage() {
   const posts = await getAllPosts();
   createPostsHTML(posts);
 }
-
+//calling the function to run the function
 allPostsPage();
