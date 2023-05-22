fcb880acc2afa0feee8cbe8c169c3b373297a8f8
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-05-22 12:11:14 +0200

    submit

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("contactForm");
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", async function(event){
+  event.preventDefault();
+
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const subject = document.getElementById("subject").value.trim();
@@ -10,37 +12,38 @@ form.addEventListener("submit", function(event){
 
   //validating the name field
   if (name.length < 6) {
-    errorMessage += "The name must be at least 6 characters long."
+    errorMessage += "The name must be at least 6 characters long.\n";
   }
 
   //validating the email field
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
-    errorMessage += "The email adress must be valid, e.g. test1@email.com";
+    errorMessage += "The email adress must be valid, e.g. test1@email.com\n";
   }
 
   //validating the subject field
   if (subject.length < 16) {
-    errorMessage += "The subject must be at least 16 characters long";
+    errorMessage += "The subject must be at least 16 characters long.\n";
   }
 
   //validating the message field
   if (message.length < 26) {
-    errorMessage += "The message must be at least 26 characters long";
+    errorMessage += "The message must be at least 26 characters long\n";
   }
 
-  //if there is an errormessage appearing, prevent the form from submitting and tisplay this error message to let the user kno
+  //if there us an error message, prevent form submission and display the error message
   if (errorMessage !== "") {
-    event.preventDefault();
     alert(errorMessage);
+  } else {
+    //simulate form submission by waiting for a few seconds (replace this with your actual form submission logic)
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    //display the success message in the submitMessage div
+    const submitMessage = document.getElementById('submitMessage');
+    submitMessage.textContent = 'Form submitted successfully!';
+    submitMessage.style.display = 'block';
+    submitMessage.style.color = 'green';
+    submitMessage.style.textTransform = 'uppercase';
+    submitMessage.style.borderRadius = '10px';
   }
 });
-
-
-/* In this code, we first get the values of the input fields by their IDs and trim any leading or trailing whitespace from the input.
-
-Then, we validate each input field using if statements and regular expressions. If an input field does not meet the requirements, we append an error message to a string variable called errorMessage.
-
-Finally, we check if the errorMessage is not empty. If it is not, we prevent the form submission and display an alert with the error message.
-
-You can add this JavaScript code to the bottom of your HTML file, just before the closing </body> tag. */
