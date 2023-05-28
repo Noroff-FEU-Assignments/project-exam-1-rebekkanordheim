f1f3ec49669824b7d46288accc1859f631e00543
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-05-28 11:40:15 +0200

    completly done

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,16 @@
-const nav = document.querySelector('nav');
-        const hamburgerMenu = document.getElementById('hamburger-menu');
+// JavaScript code to close the navigation menu when clicked outside
 
-        // Add a click event listener to the document
-        document.addEventListener('click', function(event) {
-            const target = event.target;
+  // Get the navigation menu and the hamburger menu checkbox element
+  const nav = document.querySelector('nav');
+  const hamburgerMenu = document.getElementById('hamburger-menu');
 
-            // Check if the clicked target is outside the navigation menu and hamburger menu checkbox
-            if (!nav.contains(target) && target !== hamburgerMenu) {
-                // Uncheck the hamburger menu checkbox to hide the navigation menu
-                hamburgerMenu.checked = false;
-            }
-        });
+  // Add a click event listener to the document
+  document.addEventListener('click', function(event) {
+    const target = event.target;
+
+    // Check if the clicked target is outside the navigation menu and hamburger menu checkbox
+    if (!nav.contains(target) && target !== hamburgerMenu && hamburgerMenu.checked) {
+      // Uncheck the hamburger menu checkbox to hide the navigation menu
+      hamburgerMenu.checked = false;
+    }
+  });
